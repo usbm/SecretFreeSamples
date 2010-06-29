7422d6773df974e5041220321d30b3dbdfa36b4c
Chase Martyn <cmartyn@gmail.com>
2010-06-29 15:12:24 -0500

    adding converted scope to clicks, adding stats to admin panel

--- app/models/click.rb
+++ app/models/click.rb
@@ -1,4 +1,8 @@
 class Click < ActiveRecord::Base
   belongs_to :visit
   belongs_to :offer
+  
+  scope :converted, lambda {
+    where(:converted => true)
+  }
 end
--- app/models/style.rb
+++ app/models/style.rb
@@ -1,6 +1,7 @@
 class Style < ActiveRecord::Base
   has_many :style_files
   has_many :visits
+  has_many :clicks, :through => :visits
   
   validates_presence_of :name
   validates_presence_of :code
